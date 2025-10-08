63ae44ade011f05d42afa553acab096368e29669
Anish Kumar Singh <anish.singh7922@gmail.com>
2025-10-08 13:12:27 +0530

    verify user otpmodel

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -263,6 +263,8 @@ const verifyLoggedInUser = async (req, res) => {
 
     await user.save();
 
+    await otpModel.findByIdAndDelete(otpData._id);
+
     return res.status(200).json({ message: "User verified successfully" });
   } catch (error) {
     return res.status(400).json({ error: error.message });
